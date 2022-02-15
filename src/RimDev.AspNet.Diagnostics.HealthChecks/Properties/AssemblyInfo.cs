f42fa7a390c416f107d28260c2fb3ef5e3b29b17
Thomas Harold <tgharold@gmail.com>
2022-02-15 08:33:02 -0500

    Yes, it's actually 2022 already

--- src/RimDev.AspNet.Diagnostics.HealthChecks/Properties/AssemblyInfo.cs
+++ src/RimDev.AspNet.Diagnostics.HealthChecks/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("ASP.NET full framework implementation of ASP.NET Core health checks.")]
 [assembly: AssemblyCompany("Ritter Insurance Marketing")]
 [assembly: AssemblyProduct("RimDev.AspNet.Diagnostics.HealthChecks")]
-[assembly: AssemblyCopyright("Copyright © 2019-2021 Ritter Insurance Marketing")]
+[assembly: AssemblyCopyright("Copyright © 2019-2022 Ritter Insurance Marketing")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("5ba7da7c-bf46-4064-ab4f-50f4165e70f3")]
